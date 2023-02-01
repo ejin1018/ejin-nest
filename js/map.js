8be05bd457a703f2ef73bc87c4cfe3c8ca285b8e
ejin1018 <amy1018@naver.com>
2023-02-01 22:50:16 +0900

    주소 p태그 삭제

--- js/map.js
+++ js/map.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded',function(){
       position: markerPosition
   });
   marker.setMap(map);
-  var iwContent = '<div class="map-marker"><p class="map-name">네스트호텔</p><p class="marker-address">인천광역시 중구 영종해안남로</p><a href="https://map.kakao.com/?urlX=373513.0&urlY=1090917.0&name=Nest+Hotel" target="_blank" class="marker-btn">큰지도보기</a> <a href="https://map.kakao.com/link/to/Nest Hotel,37.4253, 126.4284" target="_blank" class="marker-btn">길찾기</a></div>',
+  var iwContent = '<div class="map-marker"><p class="map-name">네스트호텔</p><a href="https://map.kakao.com/?urlX=373513.0&urlY=1090917.0&name=Nest+Hotel" target="_blank" class="marker-btn">큰지도보기</a> <a href="https://map.kakao.com/link/to/Nest Hotel,37.4253, 126.4284" target="_blank" class="marker-btn">길찾기</a></div>',
       iwPosition = new kakao.maps.LatLng(37.4253, 126.4284); //인포윈도우 표시 위치입니다
   var infowindow = new kakao.maps.InfoWindow({
       position : iwPosition, 
